d8bf91df2f116ba1e70a1e88de226dd66eff79b5
jhegner <jidlafehegner@gmail.com>
2022-01-30 00:14:50 -0300

    utilizacao de parametros opcionais

--- src/test/java/br/com/jhegnerlabs/Runner.java
+++ src/test/java/br/com/jhegnerlabs/Runner.java
@@ -1,9 +1,14 @@
 package br.com.jhegnerlabs;
 
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = "pretty",
+        snippets = CucumberOptions.SnippetType.UNDERSCORE
+)
 public class Runner {
 
 
